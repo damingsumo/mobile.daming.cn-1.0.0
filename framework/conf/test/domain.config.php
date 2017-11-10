e972c895d117db10c205493e8064d1dcd0ed24be
zhangshenshen <zss931576230@qq.com>
2017-11-10 16:16:51 +0800

    no message

--- framework/conf/test/domain.config.php
+++ framework/conf/test/domain.config.php
@@ -5,6 +5,6 @@
  */
 
 //主域名
-define('DOMAIN', 'ztit.com.cn');
+define('DOMAIN', 'bestdaming.cn');
 //访问地址
-define('HOME_URL', 'http://test.mgr.match.'.DOMAIN.'/');
+define('HOME_URL', 'http://test.mobile.'.DOMAIN.'/');
