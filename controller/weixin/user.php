7e086e97713b3b0950cf019a027571148269c925
zhangshenshen <zss931576230@qq.com>
2018-02-06 18:28:18 +0800

    no message

--- controller/weixin/user.php
+++ controller/weixin/user.php
@@ -68,7 +68,7 @@ class Controller_Weixin_User extends Controller_Base {
         if(strpos($tmpNick, 'emoji') !== false ){
             $nickName = '';
         }
-    
+    print_r($_REQUEST);exit;
         //第五步 用户信息写入session
         $weixinUser = WebApi_User_Weixin::instance()->getUserByOpenId($openid);
         if(empty($weixinUser)) {
