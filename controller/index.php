82ba808c7b7f07b9d2770a8932b8f401b130b181
zhangshenshen <zss931576230@qq.com>
2018-02-06 17:26:50 +0800

    no message

--- controller/index.php
+++ controller/index.php
@@ -83,12 +83,9 @@ class Controller_Index extends Controller_Base {
 	 */
 	public function index() {
 	    $appid=WEIXIN_APPID;
-	    
+	    $redirect_uri = urlencode ('http://test.mobile.bestdaming.cn/getUserInfo.php');
 // 	    print_r($redirect_uri);exit;
-//         $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri='http://test.mobile.bestdaming.cn/getUserInfo'&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
-	    $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
-	    
-	    
+        $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
 	    header("location:".$url);
 	    
 	    
