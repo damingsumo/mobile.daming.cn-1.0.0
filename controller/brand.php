bb05eef1f8ceb4c1802b665de03eaa520932c976
zhangshenshen <zss931576230@qq.com>
2018-02-07 17:27:51 +0800

    no message

--- controller/brand.php
+++ controller/brand.php
@@ -9,7 +9,7 @@ class Controller_Brand extends Controller_Base {
         // 	    print_r($redirect_uri);exit;
         $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
         
-        $data = WeiXin_Http::get($url);
+        $data = curl::curl_request($url);
         
         print_r($_SESSION);exit;
         
