0c664e0a08477e224d55b67e3eb499bb78234d33
zhangshenshen <zss931576230@qq.com>
2018-02-07 13:56:40 +0800

    no message

--- coreapi/user.php
+++ coreapi/user.php
@@ -69,6 +69,7 @@ class CoreApi_User extends CoreApi {
 	public function getUserByOpenId($openid) {
 	    $sql = 'select * from ' . $this->_tableName . ' where 1 ';
 	    $sql .= 'open_id = '.$openid;
+	    print_r($this->db->select_one($sql););exit;
 	    return $this->db->select_one($sql);
 	}
 	
