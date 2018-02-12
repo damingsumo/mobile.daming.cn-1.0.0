fd8ea15511034c9e9a57e3fb35212c457357c09a
zhangshenshen <zss931576230@qq.com>
2018-02-12 17:02:04 +0800

    no message

--- framework/conf/global.config.php
+++ framework/conf/global.config.php
@@ -29,7 +29,7 @@ define ( 'LONG_PRICE_ID', 10072 );
 define('CHARSET', 'utf-8');
 //赛事接口
 define('MGR_DOMIAN', 'dev.mgr.daming.com.cn');
-define('PAY_DOMIAN', 'pag.bestdaming.cn');
+define('PAY_DOMIAN', 'pay.bestdaming.cn');
 //日志目录
 define('LOGS_DIR', '/home/logs/');
 //网站安全密钥
